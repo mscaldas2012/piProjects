c88f136b77d6953437a3cbc888dade173e0557f3
mscaldas2012 <mscaldas@gmail.com>
2015-03-29 14:38:34 -0400

    fixing typos

--- Buttons/pwmStudy.py
+++ Buttons/pwmStudy.py
@@ -9,6 +9,7 @@ dutyCycle = 50
 freqDelta = 5
 dutyCycleDelta = 5
 
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup (LED, GPIO.OUT)
 pwm = GPIO.PWM(LED, freq)
 pwm.start(dutyCycle)
@@ -17,12 +18,12 @@ remote = WiiRemote()
 
 
 def changeFreq(newFreq):
-    print 'Freq changed to ' + newFreq
+    print 'Freq changed to ' + str(newFreq)
     pwm.ChangeFrequency(newFreq)
 
 
 def changeDutyCycle(newDC):
-    print 'Duty Cycle changed to ' + newDC
+    print 'Duty Cycle changed to ' + str(newDC)
     pwm.ChangeDutyCycle(newDC)
 
 try:
@@ -36,7 +37,7 @@ try:
         if remote.arrowLeftPressed():
             dutyCycle -= dutyCycleDelta
             changeDutyCycle(dutyCycle)
-        if remote.arrowRigthPressed():
+        if remote.arrowRightPressed():
             dutyCycle += dutyCycleDelta
             changeDutyCycle(dutyCycle)
 
